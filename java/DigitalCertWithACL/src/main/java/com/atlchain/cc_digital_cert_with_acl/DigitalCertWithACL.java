750fbe0389474ef026374751a11c56b559725b97
stone-ch <great_cy_ang@163.com>
2019-11-11 21:07:57 +0800

    add DigitalCertWithACL Chaincode

--- java/DigitalCertWithACL/src/main/java/com/atlchain/cc_digital_cert_with_acl/DigitalCertWithACL.java
+++ java/DigitalCertWithACL/src/main/java/com/atlchain/cc_digital_cert_with_acl/DigitalCertWithACL.java
@@ -6,14 +6,11 @@ import org.apache.commons.logging.LogFactory;
 import org.hyperledger.fabric.shim.ChaincodeBase;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 import org.hyperledger.fabric.shim.ledger.KeyModification;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
 import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.List;
 
@@ -21,28 +18,50 @@ public class DigitalCertWithACL extends ChaincodeBase {
     private static Log _logger = LogFactory.getLog(com.atlchain.cc_digital_cert_with_acl.DigitalCertWithACL.class);
 
     public static void main(String[] args) {
+        System.setProperty("file.encoding","UTF-8");
+
+        try {
+            Field charset = Charset.class.getDeclaredField("defaultCharset");
+            charset.setAccessible(true);
+            charset.set(null,null);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
         System.out.println("OpenSSL avaliable: " + OpenSsl.isAvailable());
         new com.atlchain.cc_digital_cert_with_acl.DigitalCertWithACL().start(args);
     }
 
     @Override
     public Response init(ChaincodeStub stub) {
+        _logger.info("Init java simple chaincode");
+        String func = stub.getFunction();
+        if (!func.equals("init")) {
+            return newErrorResponse("function other than init is not supported");
+        }
+
         return newSuccessResponse();
     }
 
     @Override
     public Response invoke(ChaincodeStub stub) {
         try {
-            _logger.info("Invoke java BCGIS chaincode");
+            _logger.info("Invoke java aclcc chaincode");
             String func = stub.getFunction();
+            System.out.println("invoke Function: " + func);
+
             List<String> params = stub.getParameters(); // include function name
             List<byte[]> paramsByte = stub.getArgs();   // not include function name
 
             if (func.equals("PutRecord")) {
                 return putRecord(stub, params);
             }
-            if (func.equals("PutRecordBytes")) {
-                return putRecordBytes(stub, paramsByte);
+//            if (func.equals("PutRecordBytes")) {
+//                return putRecordBytes(stub, paramsByte);
+//            }
+            if (func.equals("GetRecordBySelector")) {
+                return getRecordBySelector(stub, params);
             }
             if (func.equals("GetRecordByKey")) {
                 return getRecordByKey(stub, params);
@@ -56,6 +75,28 @@ public class DigitalCertWithACL extends ChaincodeBase {
         }
     }
 
+    private Response putRecord(ChaincodeStub stub, List<String> args){
+        int argsNeeded = 2;
+        if (args.size() != argsNeeded){
+            return newErrorResponse("Incorrect number of arguments.Got" + args.size() + ", Expecting " + argsNeeded);
+        }
+        String putKey = args.get(0);
+        String jsonStr = args.get(1);
+
+        System.out.println("putKey: " + putKey);
+        System.out.println("jsonStr: " + jsonStr);
+
+        // get regioncode
+        byte[] userByte = stub.getCreator();
+        String regionCode = Utils.getRegioncode(userByte);
+        System.out.println("regioncode:" + regionCode);
+        String key = regionCode + putKey;
+        System.out.println("key:" + key);
+
+        stub.putStringState(key, jsonStr);
+        return newSuccessResponse("Invoke finished successfully." );
+    }
+
     private Response putRecordBytes(ChaincodeStub stub, List<byte[]> args){
         int argsNeeded = 3;
         if (args.size() != 3){
@@ -65,6 +106,72 @@ public class DigitalCertWithACL extends ChaincodeBase {
         byte[] byteArray = args.get(2);
         stub.putState(putKey, byteArray);
         return newSuccessResponse("Invoke finished successfully");
+    }
+
+    private Response getRecordBySelector(ChaincodeStub stub, List<String> args) {
+        int argsNeeded = 1;
+        if (args.size() != argsNeeded){
+            return newErrorResponse("Incorrect number of arguments.Got" + args.size() + ", Expecting " + argsNeeded);
+        }
+
+        byte[] userByte = stub.getCreator();
+        String regioncode = Utils.getRegioncode(userByte);
+        System.out.println("regionCode: " +  regioncode);
+
+        //queryString := "{\"selector\":{\"Hash\":\"" + hash + "\"}}"
+        String queryString = "{\"selector\":" + args.get(0)+ "}";
+        System.out.println("queryString: " + queryString);
+        QueryResultsIterator<KeyValue> queryResultsIterator = stub.getQueryResult(queryString);
+        Iterator<KeyValue> iterator = queryResultsIterator.iterator();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+
+        boolean bArrayMemberAlreadyWritten = false;
+        while (iterator.hasNext()) {
+            KeyValue kv = iterator.next();
+            System.out.println("kv :" + kv.getKey());
+            if (!Utils.isAccessable(regioncode, kv.getKey().substring(0, 6))) {
+                continue;
+            }
+
+            if (bArrayMemberAlreadyWritten == true) {
+                stringBuilder.append(",");
+            }
+            stringBuilder.append("{\"Key\":\"");
+            stringBuilder.append(kv.getKey());
+            stringBuilder.append("\", \"Record\":");
+            stringBuilder.append(kv.getStringValue());
+            stringBuilder.append("}");
+            bArrayMemberAlreadyWritten = true;
+        }
+        stringBuilder.append("]");
+        System.out.println("result: " + stringBuilder.toString());
+        return newSuccessResponse("Successfully", stringBuilder.toString().getBytes());
+    }
+
+    private Response getRecordByKey(ChaincodeStub stub, List<String> args) {
+        int argsNeeded = 1;
+        if (args.size() != argsNeeded){
+            return newErrorResponse("Incorrect number of arguments.Got" + args.size() + ", Expecting " + argsNeeded);
+        }
+        String key = args.get(0);
+        byte[] tmpVal = stub.getState(key);
+        byte[] val = null;
+
+        System.out.println("getState: " + new String(tmpVal));
+
+        // get regioncode
+        byte[] userByte = stub.getCreator();
+        String regioncode = Utils.getRegioncode(userByte);
+
+        if (Utils.isAccessable(regioncode, key.substring(0, 6))) {
+            val = tmpVal;
+        } else {
+            return newErrorResponse(String.format("Error: state for %s is null", key));
+        }
+        String message = "Query key->\"" + key + "\" successfully";
+        return newSuccessResponse(message, val);
     }
 
     private Response getHistoryByKey(ChaincodeStub stub, List<String> args) {
@@ -92,90 +199,5 @@ public class DigitalCertWithACL extends ChaincodeBase {
         String message = "Query history of key->\"" + key + "\" successfully";
         return newSuccessResponse(message, strBuilder.toString().getBytes());
     }
-
-    private String getOu(byte[] userByte){
-//        byte[] userByte = stub.getCreator();
-        Response response = newSuccessResponse(userByte);
-        String initString = response.getStringPayload();
-        String[] strings = initString.split("\n");
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String s : strings) {
-            if(s.contains("BEGIN CERTIFICATE")){
-                stringBuilder.append("-----BEGIN CERTIFICATE-----" + "\n");
-            }else {
-                stringBuilder.append(s + "\n");
-            }
-        }
-//        System.out.println(stringBuilder.toString());
-        String ou = null;
-        String tmpString;
-        String textContent;
-        try {
-            InputStream inputStream = new ByteArrayInputStream(stringBuilder.toString().getBytes());
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            X509Certificate certificate = (X509Certificate) cf.generateCertificate(inputStream);
-            tmpString = certificate.getSubjectDN().toString();
-            List<String> list = Arrays.asList(tmpString.split(","));
-            textContent = list.get(1);
-            textContent = textContent.trim();
-            while (textContent.startsWith("　")) {
-                textContent = textContent.substring(1, textContent.length()).trim();
-            }
-            ou = textContent.substring(3, textContent.length());
-        }catch (CertificateException e) {
-            e.printStackTrace();
-        }
-        System.out.println(ou);
-        return ou;
-    }
-
-    private Response putRecord(ChaincodeStub stub, List<String> args){
-        int argsNeeded = 2;
-        if (args.size() != argsNeeded){
-            return newErrorResponse("Incorrect number of arguments.Got" + args.size() + ", Expecting " + argsNeeded);
-        }
-        // get ou
-        byte[] userByte = stub.getCreator();
-        String ou = getOu(userByte);
-        System.out.println("ou: " + ou);
-
-        String key = args.get(0);
-        String value = args.get(1);
-        stub.putStringState(key, value);
-        return newSuccessResponse("Invoke finished successfully." );
-    }
-
-    private Response getRecordByKey(ChaincodeStub stub, List<String> args) {
-        int argsNeeded = 2;
-        if (args.size() != argsNeeded){
-            return newErrorResponse("Incorrect number of arguments.Got" + args.size() + ", Expecting " + argsNeeded);
-        }
-        String key = args.get(0);
-        String OU = args.get(1);
-        byte[] tmpVal = stub.getState(key);
-        byte[] val = null;
-
-        // get ou
-        byte[] userByte = stub.getCreator();
-        String ou = getOu(userByte);
-        System.out.println("ou: " + ou);
-
-        if( "0000".equals(OU.substring(2, 6)) && OU.substring(0, 2).equals(key.substring(0, 2))){
-            System.out.println("Provincial level viewing data");
-            val = tmpVal;
-        }else if("00".equals(OU.substring(4, 6)) && OU.substring(0, 4).equals(key.substring(0, 4))){
-            System.out.println("City level view data");
-            val = tmpVal;
-        }else if(OU.substring(0, 6).equals(key.substring(0, 6))){
-            System.out.println("County level view data");
-            val = tmpVal;
-        }else{
-            System.out.println("The certificate does not match, please enter the correct code");
-        }
-        if (val == null) {
-            return newErrorResponse(String.format("Error: state for %s is null", key));
-        }
-        String message = "Query key->\"" + key + "\" successfully";
-        return newSuccessResponse(message, val);
-    }
 }
+// 99ad40afa494c305c12e8d07b25221c152848f2806d33837aa6eba906b39b281
---
+++ java/DigitalCertWithACL/src/main/java/com/atlchain/cc_digital_cert_with_acl/Utils.java
@@ -0,0 +1,58 @@
+package com.atlchain.cc_digital_cert_with_acl;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+public class Utils {
+//    public static void main(String[] args) {
+//        boolean access = isAccessable("140000", "140130");
+//        System.out.println(access);
+//    }
+
+    public static String getRegioncode(byte[] userByte) {
+        String certStr = new String(userByte);
+        System.out.println("certStr: \n" + certStr);
+        String[] strings = certStr.split("\n");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : strings) {
+            if (s.contains("BEGIN CERTIFICATE")) {
+                stringBuilder.append("-----BEGIN CERTIFICATE-----" + "\n");
+            } else {
+                stringBuilder.append(s + "\n");
+            }
+        }
+
+        String regioncode = null;
+        try {
+            InputStream inputStream = new ByteArrayInputStream(stringBuilder.toString().getBytes());
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate = (X509Certificate) cf.generateCertificate(inputStream);
+            byte[] bytes = certificate.getExtensionValue("1.2.3.4.5.6.7.8.1");
+            String extValue = new String(bytes);
+            String str = extValue.split(",")[3];
+            regioncode = str.substring(str.length() - 9, str.length() - 3);
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+        return regioncode;
+    }
+
+    public static boolean isAccessable(String regionCode, String keyCode) {
+        if( "0000".equals(regionCode.substring(2, 6)) && regionCode.substring(0, 2).equals(keyCode.substring(0, 2))){
+            System.out.println("Provincial level viewing data");
+            return true;
+        }else if("00".equals(regionCode.substring(4, 6)) && regionCode.substring(0, 4).equals(keyCode.substring(0, 4))){
+            System.out.println("City level view data");
+            return true;
+        }else if(regionCode.substring(0, 6).equals(keyCode)){
+            System.out.println("County level view data");
+            return true;
+        }else{
+            System.out.println("The certificate does not match, please enter the correct code");
+            return false;
+        }
+    }
+}
